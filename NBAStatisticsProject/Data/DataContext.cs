b4db26a440600a8a67d9d2bc3006605768a56440
Nikola Maravic <nikolamaravic7@gmail.com>
2026-07-22 18:24:57 +0200

    Use 'timestamp without time zone' for DateTime columns

--- NBAStatisticsProject/Data/DataContext.cs
+++ NBAStatisticsProject/Data/DataContext.cs
@@ -18,6 +18,17 @@ namespace NBAStatisticsProject.Data
         {
             base.OnModelCreating(modelBuilder);
 
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                foreach (var property in entityType.GetProperties())
+                {
+                    if (property.ClrType == typeof(DateTime) || property.ClrType == typeof(DateTime?))
+                    {
+                        property.SetColumnType("timestamp without time zone");
+                    }
+                }
+            }
+
             modelBuilder.Entity<Game>()
                 .HasOne(g => g.HomeTeam)
                 .WithMany(t => t.HomeGames)
